018baa627cb8bcf6c09b7c28b1772a5ab9fcb32e
natalia galindo <nataliadgalindo@outlook.com>
2023-05-19 21:55:20 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis
+        aspernatur repudiandae iusto repellat vero laboriosam nostrum. Deleniti,
+        excepturi iure modi repellat soluta amet exercitationem ratione neque
+        doloremque voluptatibus ipsum, commodi ea vero sint architecto dolores
+        atque? Labore, corporis optio.
+      </p>
+    </section>
+  )
+}
+
+export default About
